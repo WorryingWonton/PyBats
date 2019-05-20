be3b1dab5bbf1ab33993d803f714b5e943ff5b47
Alan Campbell <ahcampbellster@gmail.com>
2019-05-20 12:18:40 -0700

    Added set based solution for mergeTwo to AP-1

--- ap_1.py
+++ ap_1.py
@@ -1,7 +1,19 @@
-#https://codingbat.com/prob/p100369
+def mergeTwo(a, b, n):
+    return sorted(set(a) | set(b))[:n]
+
 def commonTwo(a, b):
     return len(set(a) | set(b)) - len(set(a) ^ set(b))
 
+
+assert mergeTwo(["a", "c", "z"], ["b", "f", "z"], 3) == ["a", "b", "c"]
+assert mergeTwo(["a", "c", "z"], ["c", "f", "z"], 3) == ["a", "c", "f"]
+assert mergeTwo(["f", "g", "z"], ["c", "f", "g"], 3) == ["c", "f", "g"]
+assert mergeTwo(["a", "c", "z"], ["a", "c", "z"], 3) == ["a", "c", "z"]
+assert mergeTwo(["a", "b", "c", "z"], ["a", "c", "z"], 3) == ["a", "b", "c"]
+assert mergeTwo(["a", "c", "z"], ["a", "b", "c", "z"], 3) == ["a", "b", "c"]
+assert mergeTwo(["a", "c", "z"], ["a", "c", "z"], 2) == ["a", "c"]
+assert mergeTwo(["a", "c", "z"], ["a", "c", "y", "z"], 3) == ["a", "c", "y"]
+assert mergeTwo(["x", "y", "z"], ["a", "b", "z"], 3) == ["a", "b", "x"]
 
 assert commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) == 2
 assert commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) == 3
